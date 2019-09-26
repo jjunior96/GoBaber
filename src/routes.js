c19fb6beb4b7ca72e666763adde990950b606f5c
Junior Alves <jjalves96@gmail.com>
2019-09-26 10:14:36 -0300

    Hash da senha do usuario

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Junior Alves',
-    email: 'teste1@email.com',
-    password_hash: '1234',
-  });
-
-  res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
