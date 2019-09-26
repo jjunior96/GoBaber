622fb02fc55c43fc1d689e0d4a7c84a734cfacd0
Junior Alves <jjalves96@gmail.com>
2019-09-26 09:40:22 -0300

    Conexão com o Banco de Dados e Cadastro de usuario teste

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  res.json({ message: 'Hello Word' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Junior Alves',
+    email: 'teste1@email.com',
+    password_hash: '1234',
+  });
+
+  res.json(user);
 });
 
 export default routes;
