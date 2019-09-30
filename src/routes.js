452c1fa4f3b6ac49b93f5c704017a319c0da05ad
Junior Alves <jjalves96@gmail.com>
2019-09-30 08:02:11 -0300

    Retorna o avatar do Backend para o Frontend

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionCrontroller from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -15,8 +17,7 @@ routes.post('/sessions', SessionCrontroller.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.get('/providers', ProviderController.index);
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
