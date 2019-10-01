02a68d3211a56af9c2404cdab3ece0c373986aef
Junior Alves <jjalves96@gmail.com>
2019-10-01 05:27:12 -0300

    Listagem de agendamentos para o provider, ordenando por data, lista por data atual

---
+++ src/app/controllers/ScheduleController.js
@@ -0,0 +1,36 @@
+import { startOfDay, endOfDay, parseISO, startOfHour } from 'date-fns';
+import { Op } from 'sequelize';
+import User from '../models/User';
+import Appointment from '../models/Appointment';
+
+class SheduleController {
+  async index(req, res) {
+    const checkUserProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!checkUserProvider) {
+      return res
+        .status(401)
+        .json({ error: 'Usuario nao é um provedor de serviço' });
+    }
+
+    const { date } = req.query;
+    const parsedDate = parseISO(date);
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfHour(parsedDate), endOfDay(parsedDate)],
+        },
+      },
+      order: ['date'],
+    });
+
+    return res.json(appointments);
+  }
+}
+
+export default new SheduleController();
--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import authMiddleware from './app/middlewares/auth';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -18,9 +19,14 @@ routes.post('/sessions', SessionCrontroller.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
 routes.get('/providers', ProviderController.index);
+
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+
+routes.get('/schedule', ScheduleController.index);
+
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
