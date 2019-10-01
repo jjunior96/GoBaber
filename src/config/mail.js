e5abaa0c39796f62e0fbdcd0fd1de2c6697785e5
Junior Alves <jjalves96@gmail.com>
2019-10-01 07:55:41 -0300

    Envio de email de cancelamento com o MailTrap

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'bde0a2f51c6814',
+    pass: '27d15ba37a4d31',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com',
+  },
+};
