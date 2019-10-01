38e1f65d84552e96e398ec7d21c37377b87ea8f7
Junior Alves <jjalves96@gmail.com>
2019-10-01 06:05:46 -0300

    Notificações de agendamentos utilizando o MongoDB

---
+++ README.md
@@ -0,0 +1,13 @@
+Rodar a aplicaçao 
+
+- Rodar o container com o Postgres
+  - docker start desafio-02
+
+- Rodar o container com o MongoDB
+  - docker start mongobarber
+
+- Abrir o PostBird
+  - port: 5432
+  - username: postgres
+  - senha: docker
+  Após logado, database meetapp
--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,10 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
+import Notification from '../schemas/Notification';
 
 class AppointmentContrller {
   async index(req, res) {
@@ -33,6 +35,9 @@ class AppointmentContrller {
     return res.json(appointments);
   }
 
+  /**
+   * Metodo Store
+   */
   async store(req, res) {
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
@@ -85,6 +90,21 @@ class AppointmentContrller {
       user_id: req.userId,
       provider_id,
       date,
+    });
+
+    /**
+     * Notificar prestador de serviço
+     */
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', às' H:mm'h'",
+      { locale: pt }
+    );
+
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para ${formattedDate}`,
+      user: provider_id,
     });
 
     return res.json(appointment);
