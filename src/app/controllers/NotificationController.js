6f9a2694f6ab8bf96f203899cb11cfab61f9462a
Junior Alves <jjalves96@gmail.com>
2019-10-01 07:38:47 -0300

    Cancelamento de agendamento, permitido apenas com 2 horas de antecedencia

---
+++ src/app/controllers/NotificationController.js
@@ -0,0 +1,38 @@
+import Notification from '../schemas/Notification';
+import User from '../models/User';
+
+class NotificationController {
+  async index(req, res) {
+    const checkIsProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!checkIsProvider) {
+      return res
+        .status(401)
+        .json({ error: 'Apenas provedores podem carregar notificações' });
+    }
+
+    const notification = await Notification.find({
+      user: req.userId,
+    })
+      .sort({ createdAt: -1 })
+      .limit(20);
+
+    return res.json(notification);
+  }
+
+  async update(req, res) {
+    // const notification = await Notification.findById(req.params.id);
+
+    const notification = await Notification.findByIdAndUpdate(
+      req.params.id,
+      { read: true },
+      { new: true }
+    );
+
+    return res.json(notification);
+  }
+}
+
+export default new NotificationController();
